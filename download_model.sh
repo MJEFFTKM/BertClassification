ec7dfa395b5317261f84a0f024635844d194b02a
Valentin Gromov <mjefftkm@gmail.com>
2022-05-26 23:14:21 +0300

    change path to model

--- download_model.sh
+++ download_model.sh
@@ -3,4 +3,4 @@ wget --load-cookies /tmp/cookies.txt \
 --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate \
 'https://docs.google.com/uc?export=download&id=1Wh1jW6Wy_B_le2wcea63nz9UZ5GCurFH' -O- | \
 sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Wh1jW6Wy_B_le2wcea63nz9UZ5GCurFH" \
- -O model/bert_best_clf.pt && rm -rf /tmp/cookies.txt
+ -O lib/model/bert_best_clf.pt && rm -rf /tmp/cookies.txt
